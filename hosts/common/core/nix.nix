375d1f260f9e41bb5d736d55d1ae2f695b42f1d1
VESSE Léo <leovesse@gmail.com>
2024-10-27 12:58:41 +0100

    adding caches

---
+++ hosts/common/core/nix.nix
@@ -0,0 +1,14 @@
+{ ... }: {
+  nix.settings = {
+    substituters = [
+      "https://walker.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://anyrun.cachix.org"
+    ];
+    trusted-public-keys = [
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+    ];
+  };
+}
