30638d183c775f8587d8a1aa2b6b7b0cb9b8d51b
VESSE Léo <leovesse@gmail.com>
2024-10-27 16:38:31 +0100

    adding walker module

---
+++ home/ciflire/common/optionnal/walker.nix
@@ -0,0 +1,30 @@
+{ inputs,pkgs,... }:
+{
+  programs.walker = {
+    enable = true;
+    package = inputs.walker.packages.${pkgs.system}.default;
+   # runAsService = true;
+
+    # All options from the config.json can be used here.
+    config = {
+      ui = {
+        fullscreen = true;
+        width = 800;
+        anchors = {
+          top = true;
+          bottom = true;
+        };
+      };
+      list = {
+        height = 500;
+      };
+      websearch.prefix = "?";
+      switcher.prefix = "/";
+      search = {
+        placeholder = "Search";
+
+      };
+    };
+
+  };
+}
