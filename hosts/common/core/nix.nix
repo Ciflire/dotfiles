c689b1ccfd6a637422dea90fd71ba8207439a727
VESSE Léo <leovesse@gmail.com>
2024-12-29 23:19:23 +0100

    chore: removing useless caches

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -7,13 +7,9 @@
         "flakes"
       ];
       substituters = [
-        "https://walker.cachix.org"
-        "https://walker-git.cachix.org"
         "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
-        "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
-        "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
