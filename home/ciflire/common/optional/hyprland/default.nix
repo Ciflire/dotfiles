b56d6966adb1fc177b898d9dd22d33d23fb9e5aa
VESSE Léo <leovesse@gmail.com>
2025-01-30 10:03:26 +0100

    mapping keybind for bluetooth headset

--- home/ciflire/common/optional/hyprland/default.nix
+++ home/ciflire/common/optional/hyprland/default.nix
@@ -145,6 +145,7 @@ in
         ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
         ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_SINK@ toggle"
         ", XF86AudioPlay, exec, playerctl play-pause"
+        ", XF86AudioPause, exec, playerctl play-pause"
         ", XF86AudioNext, exec, playerctl next"
         ", XF86AudioPrev, exec, playerctl previous"
       ];
