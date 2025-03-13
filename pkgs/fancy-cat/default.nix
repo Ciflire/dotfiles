e2885b713957877a855747d93345c15661fefd6d
VESSE Léo <leovesse@gmail.com>
2025-03-13 15:12:21 +0100

    feat: fancy-cat package for 0.4.0
    fix: remove comments

--- pkgs/fancy-cat/default.nix
+++ pkgs/fancy-cat/default.nix
@@ -3,10 +3,10 @@
   lib,
   stdenv,
   pkgs,
+  pkg-config,
   fetchFromGitHub,
   zig,
 }:
-
 stdenv.mkDerivation {
   pname = "fancy-cat";
   version = "0.4.0";
@@ -14,33 +14,44 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     owner = "freref";
     repo = "fancy-cat";
-    rev = "9b28f097322a0e90154f172318a504a89afcc828";
-    hash = "sha256-Wasxhsv4QhGscOEsGirabsq92963S8v1vOBWvAFuRoM=";
+    rev = "8b073a4dc20d75856f303592ca6ccd46648e2ae6";
+    hash = "sha256-ziHtPfK9GOxKF800kk+kh12Fwh91xbjDYx9wv2pLZWI=";
   };
 
-  # patches = [ ./0001-changes.patch ];
+  patches = [ ./0001-changes.patch ];
 
   nativeBuildInputs = [
-    zig.hook
+    pkg-config
+    zig
   ];
 
   buildInputs = with pkgs; [
-    zig
     mupdf
-    libz
-    mujs
-    gumbo
-    openjpeg
-    freetype
     harfbuzz
-    libjpeg
+    freetype
     jbig2dec
+    libjpeg
+    openjpeg
+    gumbo
+    mujs
+    libz
   ];
 
-  postPatch = ''
-    ln -s ${callPackage ./deps.nix { }} $ZIG_GLOBAL_CACHE_DIR/p
+  buildPhase = ''
+    # Set Zig's cache directory
+    export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+
+    zig build --release=fast --system ${callPackage ./deps.nix { }} -Dcpu="skylake"
   '';
 
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp $TMPDIR/source/zig-out/bin/fancy-cat $out/bin/
+
+    runHook postInstall
+  '';
   meta = with lib; {
     description = "PDF viewer for terminals using the Kitty image protocol";
     homepage = "https://github.com/freref/fancy-cat";
