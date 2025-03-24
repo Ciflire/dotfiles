32906f47c1b9639a798cc242995ffe9ba5c56450
VESSE Léo <leovesse@gmail.com>
2025-03-24 17:23:25 +0100

    chore: add all dev packages for svelte

--- home/ciflire/common/optional/language_setups/svelte.nix
+++ home/ciflire/common/optional/language_setups/svelte.nix
@@ -1,9 +1,14 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    bun
     nodejs_23
 
     svelte-language-server
+    vscode-langservers-extracted
+    typescript-language-server
+    nodePackages_latest.svelte-check
     nodePackages_latest.prettier
+    emmet-language-server
   ];
 }
