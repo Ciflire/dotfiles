40cf0c8efcb114925562c7198f1f16347f3cef95
VESSE Léo <leovesse@gmail.com>
2025-12-04 11:16:48 +0100

    platformio

--- hosts/common/optional/platformio.nix
+++ hosts/common/optional/platformio.nix
@@ -1,9 +1,16 @@
 { pkgs, ... }:
 {
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [ avrdude ];
+
   services.udev.packages = with pkgs; [
     platformio-core.udev
     openocd
+  ];
+
+  environment.systemPackages = with pkgs; [
+    cmake
   ];
 
 }
