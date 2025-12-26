4d7c141cce4a83aa71bba4fdf1c3a3f100544dbd
VESSE Léo <leo.vesse@pm.me>
2025-12-26 18:17:18 +0100

    change singin key id

--- hosts/common/optional/git.nix
+++ hosts/common/optional/git.nix
@@ -5,7 +5,7 @@
     config = {
       user.email = "leo.vesse@pm.me";
       user.name = "VESSE Léo";
-      user.signingkey = "728A4EAD975731AC";
+      user.signingkey = "212B1C2241F207AB";
       commit.gpgsign = true;
     };
   };
