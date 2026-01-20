e7d281862496eb9fd7bf436791ed478d5754a085
VESSE Leo <leo.vesse@pm.me>
2026-01-20 19:20:37 +0100

    test signing

--- hosts/common/optional/git.nix
+++ hosts/common/optional/git.nix
@@ -5,8 +5,10 @@
     config = {
       user.email = "leo.vesse@pm.me";
       user.name = "VESSE Léo";
-      user.signingkey = lib.mkIf (config.networking.hostName == "vivobook14") "C900ED26D95586C0";
-      commit.gpgsign = lib.mkIf (config.networking.hostName == "vivobook14") true;
+      # user.signingkey = lib.mkIf (config.networking.hostName == "vivobook14") "C900ED26D95586C0";
+      # commit.gpgsign = lib.mkIf (config.networking.hostName == "vivobook14") true;
+      user.signingkey = "C900ED26D95586C0";
+      commit.gpgsign = true;
     };
   };
 }
